8ee07dfdc30bbb993a454f8ceab2e115bfb73006
Tripti Shres <tripti.shres@gmail.com>
2020-09-17 16:26:17 +1000

    Initial file

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const htmlRoutes = require("./routes/html-routes");
+app.use(htmlRoutes);
+const apiRoutes = require("./routes/api-routes");
+app.use(apiRoutes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
